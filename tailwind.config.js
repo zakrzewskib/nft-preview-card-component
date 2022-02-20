4969c4bcfb9f1eba0c688460b905aa8c6a087a75
Bartosz Zakrzewski <barzak200@gmail.com>
2022-02-20 16:37:53 +0100

    Style main structure of the card

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        softBlue: 'hsl(215, 51%, 70%)',
+        cyan: 'hsl(178, 100%, 50%)',
+        mainBG: 'hsl(217, 54%, 11%)',
+        cardBG: 'hsl(216, 50%, 16%)',
+        line: 'hsl(215, 32%, 27%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+    },
+  },
+  plugins: [],
+};
